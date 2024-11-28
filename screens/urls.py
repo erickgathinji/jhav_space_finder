2feefc0f6a6400e47fa7165e1777fb360b939e51
Erick Gathinji <ericgathinji@gmail.com>
2024-11-28 16:40:47 +0300

    screens setup

--- screens/urls.py
+++ screens/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.index, name='index'),
 
     path('about', views.about, name='about'),
 
-}
+]
